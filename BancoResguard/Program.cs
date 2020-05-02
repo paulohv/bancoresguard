6464254f7fe3bf11f64d16a140e5b415b47142fe
paulo <paulo.herrera94@gmail.com>
2020-05-02 06:34:04 -0400

    scdfd

---
+++ BancoResguard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BancoResguard
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
